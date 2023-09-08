420c9a7d7cee55cd5f28e31bc25afd633bb38f1f
kingsiuness12 <133829859+kingsiuness12@users.noreply.github.com>
2023-09-08 12:51:38 +0900

    Add python_01_variable

---
+++ lecture/python_01_variable.py
@@ -0,0 +1,10 @@
+# 주석(Comment) : 단순 메모, 개발 실행X
+
+# 출력 함수(print)
+# - ()안의 값을 출력
+# -변수 값 확인용도 또는 메세지 출력용도
+print("F")
+# 문자열 타입(String Type)
+# -Pythom: ''."" -> String
+# - C, Java: ''(Char), ""(string)
+
